99b5a227db9e17f14a87f7fa4bef02cc0f7c7e18
Arman <mesropyanarman4@gmail.com>
2023-06-18 17:01:31 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { Button, Download, Feautures, SectionWrapper } from "./components";
+import assets from "./assets";
+import styles from "./styles/global";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="overflow-hidden">
+      <SectionWrapper
+        title="Your own store of Nifty NFTs. Start Selling & Growing"
+        description="Buy, store, collect NFTS , exchange & earn crypto. Join 25+ million people using ProNef Marketplaace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart User Interface  Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI Design"
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Feautures />
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all users devices. You can easily get yor app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner='banner02'
+      />
+      <Download/>
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}<span className="bold"> Arman Mesroyan</span></p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Button from "./Button";
+import Download from "./Download";
+import Feautures from "./Feautures";
+import SectionWrapper from "./SectionWrapper";
+
+export { Button, Download, Feautures, SectionWrapper };
